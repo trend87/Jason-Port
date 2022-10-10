0bf99ecf60996a79b0dacf9e1a7efe5bf4cefd3c
Effiong Israel Robert <israelrobert@gmail.com>
2022-10-10 17:21:51 +0100

    styled and gave mobile menu a stack order of 10

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector("#menu")
 hamBurger.addEventListener("click", function (){
     if (menu.classList.contains("hidden")){
         menu.classList.remove("hidden")
+        
        
     }else{
         menu.classList.add("hidden")
